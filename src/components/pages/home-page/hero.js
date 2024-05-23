1844d001a6ec0ae19022d8b5f62aab78cf392c93
andres-sumihe <asumihe14@gmail.com>
2024-05-24 02:58:00 +0700

    fix: using Image instead of img

--- src/components/pages/home-page/hero.js
+++ src/components/pages/home-page/hero.js
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 export default function Hero() {
     return (
         <section className="min-h-screen py-24 md:px-20 px-12 md:pt-0 grid grid-cols-1 md:grid-cols-2 align-baseline snap-start bg-[url(/images/hero-background.jpg)] bg-cover">
@@ -6,7 +8,8 @@ export default function Hero() {
                 <p className="text-lg font-thin lg:text-lg  text-gray-600 tracking-tight mb-8">Arisan Security (A-SEC) adalah wadah untuk berbicara seputar Information Technology, terkhusus pada bidang Information Technology Security. Kami sangat amat terbuka bagi semua kalangan &amp; Elemen masyarakat untuk bukan hanya sekedar berkolaborasi melainkan berelaborasi.</p>
             </div>
             <div className="hidden md:flex justify-center items-center px-0 md:px-12 py-0 mt-4 md:mt-0 md:py-24" data-aos={"zoom-in"}>
-                <img src="/images/world-cuate.svg" alt="World Cuate SVG" className="w-[540px] h-[100%]" />
+                <Image src="/images/world-cuate.svg" alt="World Cuate SVG" width={540} height={540}/>
+                {/* <img src="/images/world-cuate.svg" alt="World Cuate SVG" className="w-[540px] h-[100%]" /> */}
                 {/* <iframe className="w-[28rem] h-[18rem]" src="https://www.youtube.com/embed/aCn8cPgRFHY" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe> */}
             </div>
         </section>
